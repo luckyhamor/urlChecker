90edc30f6f9313e1e2f56cab8191f8c57c014e45
luckyhamor <90776975+luckyhamor@users.noreply.github.com>
2025-03-26 15:05:39 +0800

    Update urlChecker.py

--- urlChecker.py
+++ urlChecker.py
@@ -8,7 +8,7 @@ import csv
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill
 
-EXPECTED_DOMAIN = "https://api.payserv.net"
+EXPECTED_DOMAIN = "https://example.com"
 VIRUSTOTAL_API_KEY = "45e86953250e2b3c549a0fcd59bd1e6346168a1cb4d1f0e476b1968a0c8906fb"
 VIRUSTOTAL_URL = "https://www.virustotal.com/api/v3/urls"
 
